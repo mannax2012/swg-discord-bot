5673169d21ddb3cb80bf875be31b0c76926178a5
MrObvious <swginfinity.devs@gmail.com>
2023-10-12 07:16:37 -0700

    Removed pm2 timestamp option

--- chatbot.sh
+++ chatbot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/infinity/workspace/chatbot 
-pm2 start swgchatbot.js --time --exp-backoff-restart-delay=100 -o ./console.log -e ./error.log
+pm2 start swgchatbot.js --exp-backoff-restart-delay=100 -o ./console.log -e ./error.log
